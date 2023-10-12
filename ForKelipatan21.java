83bc40d0f1ffe385e83e74319b7516d4326734b4
rohmanalka <rohmanalka06@gmail.com>
2023-10-12 08:20:54 +0700

    penambahan variabel untuk menghitung rata-rata

--- ForKelipatan21.java
+++ ForKelipatan21.java
@@ -8,18 +8,18 @@ public class ForKelipatan21 {
         int counter = 0;
         int total = 0;
         System.out.print("Masukkan bilangan kelipatan (1-9): ");
-        int kelipatan  = scan.nextInt();
+        int kelipatan  = scan.nextInt(); 
 
         for (int i=1; i<=50; i++) {
             if (i % kelipatan == 0){
                 total += i;
                 counter++;
-
-            System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
-            System.out.printf("Total bilangan  kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
             }
         }
-
+        double rata = (double) total/counter;
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan  kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, total);
+        System.out.printf("Rata rata bilangan kelipatan %d dari 1 sampai 50 adalah %2f\n", kelipatan, rata);
     }
     
 }
